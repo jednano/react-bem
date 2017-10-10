3702c5db0c846c9add3b88f3114091427ed1c69c
Jed Mao <jedmao@outlook.com>
2017-10-09 22:42:15 -0700

    Introduce createBEMElementSFC

---
+++ src/createBEMElementSFC.ts
@@ -0,0 +1,48 @@
+import * as PT from 'prop-types'
+import * as React from 'react'
+
+import { BEMBlockProviderContext } from './createBEMBlock'
+import { getDisplayName } from './helpers'
+import resolveRenderedElement from './resolveRenderedElement'
+import { BEMElementProps } from './types'
+
+/**
+ * Wraps a class with BEM element functionality, receiving the BEM block name
+ * via context and converting element and modifiers attributes into className
+ * attributes.
+ * @param SFC The class to wrap with BEM element functionality.
+ */
+export default function createBEMElementSFC<P = {}>(
+	SFC: React.SFC<P>,
+) {
+	const WrappedSFC: React.SFC = function (
+		props: P,
+		context: BEMBlockProviderContext,
+	) {
+		return resolveRenderedElement(
+			SFC(props, context),
+			props,
+			context,
+		) as JSX.Element | null
+	}
+
+	WrappedSFC.displayName = `BEMElement(${getDisplayName(SFC as any)})`
+
+	WrappedSFC.contextTypes = {
+		...(SFC.contextTypes || {}),
+		block: PT.string.isRequired,
+	}
+
+	WrappedSFC.propTypes = {
+		...(SFC.propTypes as any || {}),
+		element: PT.string,
+		modifiers: PT.any,
+	}
+
+	return WrappedSFC as React.SFC<P & BEMElementProps> & {
+		displayName: string
+		contextTypes: typeof SFC.contextTypes & {
+			block: PT.Validator<string>
+		}
+	}
+}
